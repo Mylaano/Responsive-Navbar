e060e05350414f678548007de1da6dbce5ec6da4
Damien DRUAUX <druaux21@outlook.fr>
2023-04-01 14:47:45 +0200

    Update
    - Fix bug on reload page and add opacity anim

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const hamburgerMenu = document.querySelector('.hamburger-menu');
 
 hamburgerMenu.addEventListener('click', () => {
     const linkList = document.querySelector('.links');
-    linkList.classList.toggle('invisible');
+    linkList.classList.toggle('visible');
 })
